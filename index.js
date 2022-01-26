59519549231d759f66a2e290087d8192374f0141
Maja Savic <masavic@smart.edu.rs>
2022-01-26 10:57:07 +0100

    Added ejs and picture

--- index.js
+++ index.js
@@ -5,20 +5,21 @@ var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root', // So secure way to do this
     password: 'root',
-    database: 'world'
+    database: 'modeli'
 });
 
 var express = require('express');
 var app = express();
 
+app.use(express.static('views'));
 app.set('view engine', 'ejs');
 connection.connect();
 
 
 app.get('/', (req, res) => {
-    connection.query('SELECT * from city', function (err, rows, fields) {
+    connection.query('SELECT * from grad', function (err, rows, fields) {
         if (err) throw err;
-        res.render('index', { rows: rows });
+        res.render('index', { REZULTAT: rows });
     });
     
 });
