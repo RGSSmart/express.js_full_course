456cecd5348890d4a7260c17565d94c053bdf35d
maja <masavic@smart.edu.rs>
2022-02-02 22:11:23 +0100

    Added /ucenici endpoint

--- index.js
+++ index.js
@@ -1,26 +1,48 @@
 var mysql = require('mysql2');
 var ejs = require('ejs');
-
+var https = require('https');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root', // So secure way to do this
     password: 'root',
-    database: 'world'
+    database: 'skola'
 });
 
 var express = require('express');
+const { getMaxListeners } = require('process');
 var app = express();
 
 app.set('view engine', 'ejs');
 connection.connect();
 
-
 app.get('/', (req, res) => {
-    connection.query('SELECT * from city', function (err, rows, fields) {
-        if (err) throw err;
-        res.render('index', { rows: rows });
-    });
-    
+    res.render('index');
 });
+
+app.get('/ucenici', (req, res) => {
+    connection.query('SELECT * from ucenici limit 3', function (err, result, fields) {
+        if (err) throw err;
+        let status = { created: "no", deployed: "no" };
+
+        result.forEach((element) => {
+            check_kids_status(element, status);
+        });
+        res.render('main', { ucenici: result });
+    });
+});
+let that = this;
+function check_kids_status(element, status) {
+    if (element.gitUsername != null) {
+        https.get('https://github.com/' + element.gitUsername, function (resp) {
+
+            that.status = { created: resp.statusCode, deployed: 'yes' };
+            console.log(that.status)
+        });
+    }
+    element.gitstatus = status;
+}
+// https.get('https://' + element.gitUsername + '.github.io', (res) => {
+//     status = res.statusCode;
+// });
 app.listen(3000);
 //connection.end();
